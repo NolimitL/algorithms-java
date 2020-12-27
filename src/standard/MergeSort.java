9e04e9195d86018027c75c36facdfe5f8705b57c
NolimitL <leonidprofessor27@gmail.com>
2020-12-27 16:09:14 +0100

    merge sort algorithm is done

--- src/standard/MergeSort.java
+++ src/standard/MergeSort.java
@@ -4,53 +4,52 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] array = RandomArray.getArray(1_000_000);
+        int[] array = RandomArray.getArray(100_000);
 //        PrintArray.print(array);
-//        int[] array = new int[]{14, 12, 6, 8, 22, 10};
+//        int[] array = new int[]{14, 12, 6, 8, 22, 10, 3};
         TimeCounter.start();
-        mergeSort(array);
+        sort(array, 0, array.length - 1);
         TimeCounter.finish();
 //        PrintArray.print(array);
     }
 
-    public static void mergeSort(int[] array) {
-        int size = 1;
-        while (size < array.length){
-            for (int step = 0; step < array.length; step += size * 2) {
-                sort(array, step, step+size, size);
-            }
-            size *= 2;
+    public static void sort(int[] array, int start, int finish) {
+        if (finish > start) {
+            int middlePoint = (finish + start) / 2;
+            sort(array, start, middlePoint);
+            sort(array, middlePoint + 1, finish);
+            merge(array, start, middlePoint, finish);
         }
     }
-    private static void sort(int[] array, int elemL, int elemR, int size){
-        int firstP = elemL;
-        int secondP = elemR;
-        int[] temp = new int[size * 2];
-        for (int step = 0; step < temp.length; step++) {
-            if (elemR < array.length){
-                if (elemL >= firstP + size && elemR < secondP + size){
-                    temp[step] = array[elemR];
-                    elemR++;
-                } else if (elemR >= secondP + size && elemL < firstP + size) {
-                    temp[step] = array[elemL];
-                    elemL++;
-                } else {
-                    if (array[elemL] < array[elemR]){
-                        temp[step] = array[elemL];
-                        elemL++;
-                    }else {
-                        temp[step] = array[elemR];
-                        elemR++;
-                    }
-                }
-            }
+
+    private static void merge(int[] array, int start, int middle, int finish) {
+        int[] leftArr = new int[middle - start + 1];
+        int[] rightArr = new int[finish - middle];
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        for (int i = 0; i < leftArr.length; i++) {
+            leftArr[i] = array[start + i];
         }
-        for (int i = 0; i < temp.length; i++) {
-            if (firstP >= array.length){
-                break;
+        for (int i = 0; i < rightArr.length; i++) {
+            rightArr[i] = array[middle + 1 + i];
+        }
+        for (int i = start; i < finish + 1; i++) {
+            if (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+                if (leftArr[leftIndex] < rightArr[rightIndex]){
+                    array[i] = leftArr[leftIndex];
+                    leftIndex++;
+                }else {
+                    array[i] = rightArr[rightIndex];
+                    rightIndex++;
+                }
+            }else if(leftIndex < leftArr.length){
+                array[i] = leftArr[leftIndex];
+                leftIndex++;
+            }else if(rightIndex < rightArr.length){
+                array[i] = rightArr[rightIndex];
+                rightIndex++;
             }
-            array[firstP] = temp[i];
-            firstP++;
         }
     }
 }
--- src/standard/TestAlgorithms.java
+++ src/standard/TestAlgorithms.java
@@ -5,41 +5,40 @@ public class TestAlgorithms {
 //        System.out.println("Initial array:");
 //        PrintArray.print(array);
 
-        int[] array = RandomArray.getArray(100000);
+        int[] array = RandomArray.getArray(100_000);
         System.out.println("Bubble: ");
         TimeCounter.start();
 //        BubbleSort.bubbleSort(array);
         TimeCounter.finish();
 
-        array = RandomArray.getArray(100000);
+        array = RandomArray.getArray(100_000);
         System.out.println("Bubble bidirectional:");
         TimeCounter.start();
 //        BubbleSortReverse.bubbleSortReverse(array);
         TimeCounter.finish();
 
-        array = RandomArray.getArray(100000);
+        array = RandomArray.getArray(100_000);
         System.out.println("Selection algorithm:");
         TimeCounter.start();
 //        SelectionSort.selectionSort(array);
         TimeCounter.finish();
 
-        array = RandomArray.getArray(100000);
+        array = RandomArray.getArray(100_000);
         System.out.println("Selection algorithm bidirectional:");
         TimeCounter.start();
 //        SelectionSortTwoWays.selectionSortBidirectional(array);
         TimeCounter.finish();
 
-        array = RandomArray.getDecreasingArray(10_000_000);
+        array = RandomArray.getDecreasingArray(100_000);
         System.out.println("Quick algorithm:");
         TimeCounter.start();
         QuickSort.quickSort(array, 0, array.length-1);
         TimeCounter.finish();
 
-//        array = RandomArray.getArray(10_000_000);
-        array = RandomArray.getGrowingArray(10_000_000);
+        array = RandomArray.getDecreasingArray(100_000);
         System.out.println("Merge sort algorithm:");
         TimeCounter.start();
-        MergeSort.mergeSort(array);
+        MergeSort.sort(array, 0, array.length-1);
         TimeCounter.finish();
 
     }
